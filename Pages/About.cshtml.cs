8997d39b009953849add2cad4ffc7d00908c4a8a
shubhamchawla02 <shubham.ryan@gmail.com>
2022-04-08 01:20:54 -0400

    First Commit

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContactManager.Pages;
+
+[AllowAnonymous]
+public class AboutModel : PageModel
+{
+    private readonly ILogger<AboutModel> _logger;
+
+    public AboutModel(ILogger<AboutModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
